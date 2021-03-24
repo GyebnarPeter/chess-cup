c3a42b5c229e131eb6ad7b4148fd8e95729eff34
GyebnarPeter <gyebnarpetter@gmail.com>
2021-03-24 16:12:25 +0100

    complete task

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,39 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        List<String> resultList = new ArrayList<>();
+        List<String> pointList = new ArrayList<>();
+        Map<Integer, String> pointMap = new TreeMap<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(fileName), "UTF-8");
+            while(scanner.hasNextLine()) {
+                pointList.add(scanner.nextLine());
+            }
+            scanner.close();
+            for(String point : pointList) {
+                String[] pointToArr = point.split(",");
+                int sumPoint = Integer.parseInt(pointToArr[1]) +
+                        Integer.parseInt(pointToArr[2]) +
+                        Integer.parseInt(pointToArr[3]) +
+                        Integer.parseInt(pointToArr[4]) +
+                        Integer.parseInt(pointToArr[5]);
+                pointMap.put(sumPoint, pointToArr[0]);
+            }
+            for(int point : pointMap.keySet()) {
+                resultList.add(pointMap.get(point));
+            }
+            Collections.reverse(resultList);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        
+        return resultList;
     }
 
 }
